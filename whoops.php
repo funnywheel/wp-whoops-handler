dc1dba2c8c2ba9ddf43165aca16b9c71caf816af
Luke Kortunov <luke@kortunov.me>
2020-06-09 13:03:22 +0300

    initial commit

---
+++ whoops.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name:     Whoops Pretty Error Handler page for WP
+ * Plugin Version:  1.0
+ * Author:          Luke Kortunov <luke@kortunov.me>
+ *
+ * @link            https://github.com/filp/whoops - Original Whoops, awesome tool
+ */
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+if (!function_exists('initWhoopsHandler')) {
+    function initWhoopsHandler()
+    {
+        if (WP_DEBUG_DISPLAY) {
+            $handler = new Whoops\Handler\PrettyPageHandler();
+            $handler->setEditor('phpstorm');
+
+            $whoops = new Whoops\Run();
+            $whoops->pushHandler($handler);
+            $whoops->register();
+        }
+    }
+
+    initWhoopsHandler();
+}
